366398838a0d1e069e31644039c772772314e663
javidanhajizada <hacizade55@gmail.com>
2020-06-04 01:30:58 +0400

    Completed

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,33 @@
+package app.config;
+
+import app.model.Animal;
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean(name = "dog")
+    public Dog getDog(){
+        Dog dog = new Dog();
+        return dog;
+    }
+
+    @Bean(name = "Timer")
+    public Timer getTimer(){
+        Timer timer = new Timer();
+        return timer;
+    }
+
+
+
+
+}
